355ac30d5c0b826f779bfbedd86ceb55cebf4454
Jackson Massey <jackmassey2000@gmail.com>
2024-02-12 13:31:15 -0600

    start flag takes more input formats now, see help, also added a script runtime tracker

--- gifcmd.sh
+++ gifcmd.sh
@@ -4,10 +4,13 @@
 ## Author: jackinthebox52
 ##
 ## Usage: gifcmd.sh -s <start> -d <duration> -i <input> (Optional:) <output>
-## Example: gifcmd.sh -s 10 -d 5 -i /path/to/video.mp4 /path/to/output/clip.gif
+## Example: gifcmd.sh -s 1:15 -d 5 -i /path/to/video.mp4 /path/to/output/clip.gif
 ## Note: Output file is optional, and should be the last parameter.
-## Start and duration should be in seconds.
- 
+## Start should be of format: hh:mm:ss, mm:ss, or ss. Duration should be of format: ss.
+## Width of the gif is set to 720px, and height is set to maintain aspect ratio. These can be changed in the script (ffmpeg command).
+
+
+current_time=$(date +%s)
 
 while getopts ":s:d:i:" flag; do
     case $flag in
@@ -26,35 +29,42 @@ while getopts ":s:d:i:" flag; do
     esac
 done
 
-#additionally, get a final optional parameter, with no flag letter assosciated, and save as variable output file
 shift $((OPTIND -1))
 output=$1
 
-# Check if mandatory flags are present
 if [[ -z $seconds || -z $duration || -z $filepath ]]; then
     echo "Usage: gifcmd.sh -s <start> -d <duration> -i <input> (Optional:) <output>" >&2
-    echo "Example: gifcmd.sh -s 10 -d 5 -i /path/to/video.mp4 /path/to/output/clip.gif" >&2
+    echo "Example: gifcmd.sh -s 1:15 -d 5 -i /path/to/video.mp4 /path/to/output/clip.gif" >&2
     echo "Note: Output file is optional, and should be the last parameter." >&2
-    echo "Start and duration should be in seconds." >&2
-    exit 1
-fi
-# Check if seconds and duration are numbers
-if [[ ! $seconds =~ ^[0-9]+$ || ! $duration =~ ^[0-9]+$ ]]; then
-    echo "Invalid seconds or duration" >&2
+    echo "Start should be of format: hh:mm:ss, mm:ss, or ss. Duration should be of format: ss." >&2
     exit 1
 fi
 
-#Check if file exists, check if file is a video (by extension), check if user can read file
+#if s flag is of fomat hh:mm:ss or mm:ssm convert it to seconds, else, check if it's a number, else exit with error message
+if [[ $seconds =~ ^[0-9]+:[0-9]+:[0-9]+$ ]]; then
+    seconds=$(echo $seconds | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+elif [[ $seconds =~ ^[0-9]+:[0-9]+$ ]]; then
+    seconds=$(echo $seconds | awk -F: '{ print ($1 * 60) + $2 }')
+fi
+
+if [[ ! $seconds =~ ^[0-9]+$ ]]; then
+    echo "Invalid start time. Please provide a valid number of seconds." >&2
+    exit 1
+fi
+
+if [[ ! $duration =~ ^[0-9]+$ ]]; then
+    echo "Invalid duration. Please provide a valid number of seconds." >&2
+    exit 1
+fi
+
 if [[ ! -f $filepath || ! -r $filepath || ! $filepath =~ \.(mp4|avi|mkv|mov|flv|wmv|webm)$ ]]; then
-    echo "Invalid input file (Extension is not video, or file not found/accessible.)" >&2
+    echo "Invalid input file (Extension is not video type, or file not found/accessible.)" >&2
     exit 1
 fi
 
-#Get the name of the file without extension
 filename=$(basename $filepath)
 filename="${filename%.*}"
 
-# Check if output parameter is present, strip .gif from it
 if [[ -n $output ]]; then
     if [[ $output != *.gif ]]; then
         echo "Invalid output file extension. Please provide a .gif file." >&2
@@ -63,9 +73,7 @@ if [[ -n $output ]]; then
     filename=${output%.gif}
 fi
 
-
-# Run ffmpeg command to create gif, using seconds as start time, duration as duration, fps=15, width=720, height=-1 and filename as output
 ffmpeg -ss $seconds -t $duration -i $filepath -filter_complex "[0:v] fps=15,scale=720:-1" $filename.gif > /dev/null 2>&1
 
-echo "Gif created successfully: $filename.gif"
+echo "Gif created successfully: $filename.gif, in $(( $(date +%s) - $current_time )) seconds."
 exit 0
